9ee207ac44e53d7696f874a9298f52f1eafef178
David Boyd <dave@intuitioneducation.com.au>
2018-05-30 16:33:13 +1000

    testing function

--- src/App.js
+++ src/App.js
@@ -9,19 +9,44 @@ class LambdaDemo extends Component {
   }
 
 
+  generateHeaders() {
+    const headers = {"Content-Type": "application/json"};
+    if (netlifyIdentity.currentUser()) {
+      return netlifyIdentity.currentUser().jwt().then((token) => {
+        return {...headers, Authorization: `Bearer ${token}`};
+      })
+    }
+    return Promise.resolve(headers);
+  }
+
   handleClick = (e) => {
     e.preventDefault();
 
     this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-        .then(response => response.json())
-        .then(json => this.setState({loading: false, msg: json.msg}));
+    this.generateHeaders().then((headers) => {
+      console.log('dave')
+      fetch('/.netlify/functions/hello', {
+        method: "POST",
+        headers,
+        body: JSON.stringify({
+          text: this.state
+        })
+      })
+      .then(response => {
+        if (!response.ok) {
+          response.text().then(err => {
+            throw (err)
+          });
+        }
+      })
+      .then(() => this.setState({loading: false, text: null, success: true, error: null}))
+      .catch(err => this.setState({loading: false, success: false, error: err.toString()}))
+    })
   }
 
 
   render() {
     const {loading, msg} = this.state;
-
     return <p>
       <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button>
       <br/>
@@ -36,7 +61,7 @@ class App extends Component {
   }
 
   handleIdentity = (e) => {
-    console.log("HERE DAVE",netlifyIdentity.currentUser())
+    console.log("HERE DAVE", netlifyIdentity.currentUser())
     e.preventDefault();
     netlifyIdentity.open();
   }
@@ -48,7 +73,7 @@ class App extends Component {
           <header className="App-header">
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <p><a href="#" onClick={this.handleIdentity}>User Login</a></p>
+          <p><a href="#" onClick={this.handleIdentity}>User Stats</a></p>
           <LambdaDemo/>
         </div>
     );
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,6 +1,12 @@
 export function handler(event, context, callback) {
+
+  console.log('context.clientContext',context.clientContext)
+  if (context.clientContext) {
+    console.log('context.clientContext.user', context.clientContext.user)
+  }
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"  + " "+ context.clientContext.user})
+    body: JSON.stringify({msg: "Hello, World!"  })
   })
 }
